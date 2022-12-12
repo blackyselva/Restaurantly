f781514e35f53653e403c082948634a9526fbe2c
blackyselva <selvam17196@gmail.com>
2022-12-12 07:46:13 +0530

    update.

---
+++ main.js
@@ -0,0 +1,16 @@
+$('.test-owl').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
