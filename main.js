818c088341ac22dc74ca8c3297fee8ff6a3888c0
blackyselva <selvam17196@gmail.com>
2022-12-12 17:15:08 +0530

    update.

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
+$('.event-owl').owlCarousel({
+    loop:true,
+	autoplay:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
 $('.test-owl').owlCarousel({
     loop:true,
+	autoplay:true,
     margin:10,
     nav:true,
     responsive:{
@@ -10,7 +28,7 @@ $('.test-owl').owlCarousel({
             items:2
         },
         1000:{
-            items:3
+            items:3,
         }
     }
 })
